da9c6e76ce2da8cedef0290449d78b436416254f
Alberto Sosa <sosa.alberto98@gmail.com>
2021-11-29 13:09:03 -0800

    Updated File Names

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package Main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -18,7 +18,10 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        JDBC.makeConnection();
+        //JDBC.makeConnection();
         launch(args);
+
+
+        //JDBC.closeConnection();
     }
 }
